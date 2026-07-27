7bc088d01b222cd61e51607149787070a8f1d3f1
CheneyCChen <sin_1@qq.com>
2026-07-27 14:05:18 +0800

    Update src/stellarator_diagnostics/__init__.py

--- src/stellarator_diagnostics/__init__.py
+++ src/stellarator_diagnostics/__init__.py
@@ -3,6 +3,7 @@
 from .api import analyze, compare, scan
 from .external import read_cobra, read_dkes, read_neo
 from .model import EquilibriumData, FieldMap, Surface
+from .runners import run_cobra_solver, run_neo_solver
 
 __all__ = [
     "EquilibriumData",
@@ -13,6 +14,8 @@ __all__ = [
     "read_cobra",
     "read_dkes",
     "read_neo",
+    "run_cobra_solver",
+    "run_neo_solver",
     "scan",
 ]
-__version__ = "0.3.0"
+__version__ = "0.4.0"
