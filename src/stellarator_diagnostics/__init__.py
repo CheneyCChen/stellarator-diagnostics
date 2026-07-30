5efc9596faa9c50221cec8d3955504fbe5a6d36e
CheneyCChen <sin_1@qq.com>
2026-07-30 09:56:01 +0800

    Add Goodman QI residual diagnostics

--- src/stellarator_diagnostics/__init__.py
+++ src/stellarator_diagnostics/__init__.py
@@ -3,6 +3,7 @@
 from .api import analyze, compare, scan
 from .external import read_cobra, read_dkes, read_neo
 from .model import EquilibriumData, FieldMap, Surface
+from .qi import compute_goodman_qi, diagnose_goodman_qi
 from .runners import run_cobra_solver, run_dkes_solver, run_neo_solver
 
 __all__ = [
@@ -11,6 +12,8 @@ __all__ = [
     "Surface",
     "analyze",
     "compare",
+    "compute_goodman_qi",
+    "diagnose_goodman_qi",
     "read_cobra",
     "read_dkes",
     "read_neo",
@@ -19,4 +22,4 @@ __all__ = [
     "run_neo_solver",
     "scan",
 ]
-__version__ = "0.5.0"
+__version__ = "0.6.0"
